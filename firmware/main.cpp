c52d98484e617c6c361a3862a1a71610ab67f3d1
Markus Fischer <markus.fischer@uni-jena.de>
2023-02-08 09:19:00 +0100

    simple test program

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -7,7 +7,32 @@
 int main()
 {
     
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_2, msp430hal::gpio::Pin::p_1, msp430hal::gpio::Mode::output> module1_status;
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_2, msp430hal::gpio::Pin::p_0, msp430hal::gpio::Mode::output> module2_status;
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_3, msp430hal::gpio::Pin::p_0, msp430hal::gpio::Mode::output> module3_status;
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_3, msp430hal::gpio::Pin::p_1, msp430hal::gpio::Mode::output> module4_status;
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_3, msp430hal::gpio::Pin::p_5, msp430hal::gpio::Mode::output> module5_status;
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_3, msp430hal::gpio::Pin::p_6, msp430hal::gpio::Mode::output> module6_status;
 
+    typedef msp430hal::gpio::GPIOPin<msp430hal::gpio::Port::port_3, msp430hal::gpio::Pin::p_4, msp430hal::gpio::Mode::output> gp_led;
+
+    module1_status::init();
+    module2_status::init();
+    module3_status::init();
+    module4_status::init();
+    module5_status::init();
+    module6_status::init();
+
+    gp_led::init();
+
+    module1_status::set();
+    module2_status::set();
+    module3_status::set();
+    module4_status::set();
+    module5_status::set();
+    module6_status::set();
+
+    gp_led::set();
     for(;;);
     return 0;
 }
